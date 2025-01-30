37ab04b4106ef8972e92be5dc8bb3dfa7219084d
Nikolai Kim <nkim.bitzap@gmail.com>
2025-01-30 11:24:47 +0100

    Added tests for PermutationSequence.Indices

---
+++ FDLibNET/UnitTests/PermutationSequence_Indices.cs
@@ -0,0 +1,32 @@
+using MathNet.Numerics.LinearAlgebra;
+using FDLibNET;
+
+namespace UnitTests;
+
+public class PermutationSequence_Indices {
+
+    [Fact]
+    public void Test_HouseholderSequence_Trans_1() {
+      var seq = new PermutationSequence(0);
+
+      Assert.Empty(seq.Indices());
+    }
+
+    //--------------------------------------------------------------------------
+
+    [Fact]
+    public void Test_PermutationSequence_Indices_2() {
+      var seq = new PermutationSequence(1);
+
+      Assert.Single(seq.Indices());
+    }
+
+    //--------------------------------------------------------------------------
+
+    [Fact]
+    public void Test_PermutationSequence_Indices_3() {
+      var seq = new PermutationSequence(101);
+
+      Assert.Equal(101, seq.Indices().Length);
+    }
+}
